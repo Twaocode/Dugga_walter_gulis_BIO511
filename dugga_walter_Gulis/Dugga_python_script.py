25166db2480f7a299ecf830d471527ea30053057
Walter Gulis <walter.gulis@gmail.com>
2025-10-17 09:36:03 +0200

    updated version which has been annotated

--- dugga_walter_Gulis/Dugga_python_script.py
+++ dugga_walter_Gulis/Dugga_python_script.py
@@ -1,4 +1,4 @@
-import csv 
+import csv # not used 
 import pandas as pd
 import argparse
 import os
@@ -14,14 +14,20 @@ def existing_file(file_path):
 #argparse is a file to begin with 
 
 def hist(data):
-    df = pd.read_csv('brca_head500_genes.csv', sep=',')
-    z = df['fpkm_log2']
+    z = data
+    #defining data as variable z 
     plt.hist(z)
+    #plotting a matplotlib histogram using said defined variable
     plt.title("Distribution of gene expression")
+    #adding title
     plt.xlabel("Expression")
+    #add name for the x axis label
     plt.ylabel("Number of genes")
+    #add name for the y axis label
     plt.savefig('fpkm_distribution.png')
+    #saving figure as png under the name fpkm_distribution
     plt.show()
+    #shows how the figure looks in a temporary screen and also closes down the plot 
 #function for making, clarifying with labels and saving a histogram plot 
 
 
@@ -31,19 +37,24 @@ def main():
     parser.add_argument('-s', '--sequence', type=existing_file, required=True, help='Input a csv file')
     args = parser.parse_args()
     # Is done with the specified flags followed by the path to the file, in this case the file is in the same directory as 
-    # as the code so the filename is sufficient 
+    # as the code so the filename is sufficient, if the function for checking if it is a file is unwanted also 
+    # remove", type=existing_file, required=True" and only specified using flags but this works 
+    # finally this is also made by the teachers 
+
 
     numbers = [15, -5, -12, 7, 10, -7, 3, -10, 4]
-    k = 0
+    #importing given numbers as a list
 
+    k = 0
     for n in numbers:
         if abs(n) >= abs(10):
-            k += abs(n)
+            k += n
         else:
             continue
     print(k)
     # a for loop checking if the value of the list is the absolute value of 10 or larger 
     # if so it is adding them to an intger so as to get the sum of if those values then printing those
+    # of not is that k intger used for summing up all the values must be made beforehande
 
     j = []
     for n in numbers:
@@ -56,7 +67,7 @@ def main():
     print(j)
     # a for loop that chekis if the number in the list is smaller than 0 and if so cubing them 
     #then adding said resultig cubed value to a list, making a list of all negative valued cubed
-    #the printing said list, of note is that the list should be prepared beforehand 
+    #the printing said list, of note is that the list should be prepared beforehand which is done using j here
 
     dupes = set()
     seen = set()
@@ -78,20 +89,18 @@ def main():
 
 
     df = pd.read_csv('brca_head500_genes.csv', sep=',')
-    #print(df)
-    z = df['fpkm_log2']
-    hist(z)
+    #taking the file inputted by argparse specified using a flag in terminal and reading it as a 
+    #dataframe using pandas while aslo making sure as to not miss mentionning that the given data is comma separated
+    print(df)
+    #checking to see if import worked by checking the content of the created dataframe 
 
-
-
-
-            
-
-    
-
+    hist(df['fpkm_log2'])
+    #calling function that plots saves and annotates a histogram of note is that the coloumn studied
+    #should include 
 
 
 
 
 if __name__ == "__main__":
     main()
+#safety function, made by teachers, make sure the code imports do get naything weird 
